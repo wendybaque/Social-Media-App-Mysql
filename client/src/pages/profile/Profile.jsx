f77b07a7a578ca835e8ddae541693dbb2a0810e8
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-11-16 10:16:07 +0100

    Add follow and unfollow functionnality and it works !

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -2,6 +2,8 @@ import "./profile.scss";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
@@ -68,14 +70,20 @@ const Profile = () => {
           <div className="profileContainer">
             <div className="uInfo">
               <div className="left">
-                <a href="https://www.facebook.com/AntiigoneWB">
+                <a href="http://facebook.com">
                   <FacebookTwoToneIcon fontSize="large" />
                 </a>
-                <a href="https://www.instagram.com/wendybqe/">
+                <a href="http://facebook.com">
                   <InstagramIcon fontSize="large" />
                 </a>
-                <a href="https://www.linkedin.com/in/wendy-baqu%C3%A9/">
+                <a href="http://facebook.com">
+                  <TwitterIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
                   <LinkedInIcon fontSize="large" />
+                </a>
+                <a href="http://facebook.com">
+                  <PinterestIcon fontSize="large" />
                 </a>
               </div>
               <div className="center">
